de0796c1d8426d637bc33993ab05885d5d5323e7
Augusto Prados <augustoprados91@gmail.com>
2025-01-12 21:53:00 -0300

    Agregado de componente loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Loading from './components/Loading/Loading';
 import './App.css';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 1500);
+    }, []);
 
     return (
-        <Router>
-            <h1>HOLA MUNDO!</h1>
-        </Router>
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <Router>
+                    {/* <NavBar />
+                    <Routes location={location}>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/about' element={<AboutMe />} />
+                        <Route path='/projects' element={<MyProjects />} />
+                        <Route path='/contact' element={<Contact />} />
+                    </Routes>
+                    <Footer /> */}
+                    <h1>HOLA MUNDO1</h1>
+                </Router>
+            )}
+        </>
     );
 }
 
