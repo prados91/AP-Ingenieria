9b491135a49c4ddfe9e484cda87d2408bb7bb8ed
Augusto Prados <augustoprados91@gmail.com>
2025-01-12 21:55:57 -0300

    Modificación en loader

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,8 +1,9 @@
 import { FadeLoader } from 'react-spinners';
-import './Loading.css';
+//import './Loading.css';
+import classes from './Loading.module.css';
 const Loading = () => {
     return (
-        <div className='loading'>
+        <div className={classes.loading}>
             <FadeLoader color='#343330' height={15} loading radius={2} width={6} margin={0} />
         </div>
     );
